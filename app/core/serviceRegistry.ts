728976570ec1bb65d0449cf2f260cebae52b57a2
AliAlaa88 <alieldinalaa04@gmail.com>
2025-10-16 22:18:05 +0300

    chore(setup): update project dependencies

---
+++ app/core/serviceRegistry.ts
@@ -0,0 +1,13 @@
+import { userInfoService } from '../modules/profile/services'
+// import { tweetService } from '../modules/tweet/services'
+// import { authService } from '../modules/auth/services'
+
+// 1️⃣ Central object containing all services
+export const services = {
+    userInfoService,
+    //   tweetService,
+    //   authService,
+}
+
+// 2️⃣ Type representing all injected services
+export type Services = typeof services
