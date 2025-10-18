2569ecb69076c1ff0eb493a654152408840cf842
Abdelrahman Medhat Saber <136710727+bedosaber77@users.noreply.github.com>
2025-10-18 13:11:27 +0300

    fix: Correct User Type in Index Page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
         <h1>User</h1>
-        <p>{{ data?.map((user: any) => user.name).join(', ') }}</p>
+        <p>{{ data?.map((user: UserType) => user.name).join(', ') }}</p>
     </div>
 </template>
 
 <script setup lang="ts">
 import { useQuery } from '@tanstack/vue-query'
-
+import type { User as UserType } from '~/modules/tweets/types'
 async function getUser() {
     const response = await fetch('https://jsonplaceholder.typicode.com/users')
     const data = await response.json()
