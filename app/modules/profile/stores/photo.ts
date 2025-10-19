25897f172f3d86b4f10a136e6112ae8fc7ac8fdf
AliAlaa88 <alieldinalaa04@gmail.com>
2025-10-19 22:07:22 +0300

    feat: cover photo focus done

--- app/modules/profile/stores/photo.ts
+++ app/modules/profile/stores/photo.ts
@@ -2,11 +2,13 @@ import { defineStore } from 'pinia'
 
 interface PhotoState {
     photoUrl: string | null
+    coverUrl: string | null
 }
 
 export const useProfilePhotoStore = defineStore('profilePhoto', {
     state: (): PhotoState => ({
         photoUrl: null,
+        coverUrl: null,
     }),
 
     actions: {
@@ -16,6 +18,14 @@ export const useProfilePhotoStore = defineStore('profilePhoto', {
 
         clearPhotoUrl() {
             this.photoUrl = null
+        },
+
+        setCoverUrl(url: string) {
+            this.coverUrl = url
+        },
+
+        clearCoverUrl() {
+            this.coverUrl = null
         },
     },
 })
