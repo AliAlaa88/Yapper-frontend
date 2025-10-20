09fb8dcaea3cdd7173d1533aa7112dc243e0fad8
hagar3bdelsalam <ha2ar3bdelsalam@gmail.com>
2025-10-20 22:13:29 +0300

    feat: add mock service methods for follow and unfollow actions

--- app/modules/profile/services/userInfoService.mock.ts
+++ app/modules/profile/services/userInfoService.mock.ts
@@ -1,37 +1,71 @@
-import type { UserProfile } from '../types/user'
-const mockUsers: UserProfile[] = [
+import type { User } from '../types/user'
+
+const users: User[] = [
     {
         id: '1',
-        username: 'mock_user',
-        displayName: 'Mock User',
-        bio: 'This is a mock user.',
-        followersCount: 1234,
-        followingCount: 567,
-        verified: true,
+        username: 'me',
+        name: 'Hagar Abdelsalam',
+        followers_count: 2,
+        following_count: 1,
+        is_follower: false,
+        is_following: false,
+        is_muted: false,
+        is_blocked: false,
     },
     {
         id: '2',
-        username: 'mock_user',
-        displayName: 'Mock User',
-        bio: 'This is a mock user.',
-        followersCount: 1234,
-        followingCount: 567,
-        verified: true,
+        username: 'hagar',
+        name: 'hagar3bdelsalam',
+        followers_count: 1,
+        following_count: 1,
+        is_follower: true,
+        is_following: false,
+        is_muted: false,
+        is_blocked: false,
     },
     {
         id: '3',
-        username: 'mock_user',
-        displayName: 'Mock User',
-        bio: 'This is a mock user.',
-        followersCount: 1234,
-        followingCount: 567,
-        verified: true,
+        username: 'sarah',
+        name: 'sarah Mohamed',
+        followers_count: 0,
+        following_count: 1,
+        is_follower: false,
+        is_following: true,
+        is_muted: false,
+        is_blocked: false,
     },
 ]
 
 export const userInfoServiceMock = {
-    async getUserInfoByUsername(username: string): Promise<UserProfile> {
+    // async getMyProfile():Promise<UserProfile> {
+    //     return Promise.resolve(mockUsers.at(0) as UserProfile)
+    // },
+
+    async getUserInfoByUsername(username: string): Promise<User> {
         console.log('Mock getUserInfoByUsername called with:', username)
-        return Promise.resolve(mockUsers.find((user) => user.username === username) as UserProfile)
+        return Promise.resolve(users.find((user) => user.username === username) as User)
+    },
+
+    async getUserByID(userId: string): Promise<User> {
+        const user = users.find((user) => user.id === userId)
+        console.log(user)
+        return Promise.resolve({...user} as User)
+    },
+
+    async followUser(userId: string) {
+        const user = users.find((user) => user.id === userId)
+        if (!user) throw new Error('user not found')
+        user.is_following = true
+        user.followers_count++
+        // should increase the number of my following
+        return user
+    },
+
+    async unfollowUser(userId: string) {
+        const user = users.find((user) => user.id === userId)
+        if (!user) throw new Error('user not found')
+        user.is_following = false
+        if (user.followers_count > 0) user.followers_count--
+        return user
     },
 }
--- app/modules/profile/types/user.ts
+++ app/modules/profile/types/user.ts
@@ -1,11 +1,20 @@
 export interface UserProfile {
     id: string
+    name: string
     username: string
-    displayName: string
     bio?: string
-    avatarUrl?: string
-    coverUrl?: string
-    followersCount: number
-    followingCount: number
-    verified: boolean
+    avatar_url?: string
+    cover_url?: string
+    followers_count: number
+    following_count: number
+    country?: string
+    created_at?: string
+    verified?: boolean
+}
+
+export interface User extends UserProfile {
+    is_follower: boolean
+    is_following: boolean
+    is_muted: boolean
+    is_blocked: boolean
 }
