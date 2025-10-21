2aafdd1174c1ade4a5838b15685a76ddf917f72d
AliAlaa88 <alieldinalaa04@gmail.com>
2025-10-21 10:36:44 +0300

    feat: mock databse and server

--- app/modules/profile/services/userInfoService.mock.ts
+++ app/modules/profile/services/userInfoService.mock.ts
@@ -1,24 +1,16 @@
-import type { UserProfile } from '../types/user'
-const mockUsers: UserProfile[] = [
-    {
-        id: '0c059899-f706-4c8f-97d7-ba2e9fc22d6d',
-        name: 'Ali',
-        username: 'ali',
-        bio: 'hi there!',
-        avatar_url:
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMEOvbEUtnNDEJOwt8taSM0LOv_C6kMh31mC-PHu9uAtnDz2ndfg7v1Fl0rGKnVamjXms&usqp=CAU',
-        cover_url:
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMEOvbEUtnNDEJOwt8taSM0LOv_C6kMh31mC-PHu9uAtnDz2ndfg7v1Fl0rGKnVamjXms&usqp=CAU',
-        followers_count: 5,
-        following_count: 15,
-        country: 'Egypt',
-        created_at: '2025-10-30',
-    },
-]
+import type { User } from '../types/user'
+import { useNuxtApp } from '#app'
 
 export const userInfoServiceMock = {
-    async getUserInfoByUsername(username: string): Promise<UserProfile> {
-        console.log('Mock getUserInfoByUsername called with:', username)
-        return Promise.resolve(mockUsers.find((user) => user.username === username) as UserProfile)
+    async getUserInfoByUsername(username: string): Promise<User> {
+        const { $axios } = useNuxtApp()
+        const response = await $axios.get<User[]>('/users', {
+            params: { username },
+        })
+        const user = response.data[0]
+        if (!user) {
+            throw new Error(`User not found: ${username}`)
+        }
+        return user
     },
 }
--- app/modules/profile/services/userInfoService.real.ts
+++ app/modules/profile/services/userInfoService.real.ts
@@ -1,7 +1,7 @@
-import type { UserProfile } from '../types/user'
+import type { User } from '../types/user'
 
 export const userInfoServiceReal = {
-    async getUserInfoByUsername(username: string): Promise<UserProfile> {
+    async getUserInfoByUsername(username: string): Promise<User> {
         throw new Error(`userInfoServiceReal.getUserInfoByUsername not implemented yet${username}"`)
     },
 }
