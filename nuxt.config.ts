6eea773fb86816ce3f5fccb45c4cac1db09a7cb6
bedosaber77 <bedosaber77@gmail.com>
2025-10-25 00:39:29 +0300

    feat(i18n): Add fallback locale to nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,6 +39,7 @@ export default defineNuxtConfig({
             useCookie: true,
             cookieKey: 'i18n_redirected',
             redirectOn: 'root',
+            fallbackLocale: 'en',
         },
     } as any,
 })
