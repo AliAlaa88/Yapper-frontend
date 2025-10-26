119f5f3cbcd8ba0d509d460371e2ce5e7f7f2db3
bedosaber77 <bedosaber77@gmail.com>
2025-10-26 21:27:35 +0300

    refactor(tweets): Replace anchor tag with NuxtLink for user navigation and utilize computed properties for URLs

--- app/modules/tweets/components/Tweet/Tweet.vue
+++ app/modules/tweets/components/Tweet/Tweet.vue
@@ -5,18 +5,19 @@
     >
         <div class="flex gap-3">
             <!-- Avatar column -->
-            <a 
-                class="flex-shrink-0" 
-                @click.stop="navigateToUser"
-                :href="user.username ? `/${user.username}` : '#'"
-                @click.prevent
-            >
-                <img 
-                    :src="user.avatar" 
-                    :alt="user.name" 
-                    class="w-10 h-10 rounded-full cursor-pointer hover:brightness-95 transition-all"
-                />
-            </a>
+             <div class="h-fit">
+                 <NuxtLink 
+                     class="flex-shrink-0" 
+                     @click.stop
+                     :to="profileUrl"
+                 >
+                     <img 
+                         :src="user.avatar" 
+                         :alt="user.name" 
+                         class="w-10 h-10 rounded-full cursor-pointer hover:brightness-95 transition-all"
+                     />
+                 </NuxtLink> 
+             </div>
 
             <!-- Content column -->
             <div class="flex-1 min-w-0">
@@ -33,7 +34,8 @@ import type { Tweet as TweetType } from '~/modules/tweets/types'
 import Publisher from './subComponents/Publisher/Publisher.vue'
 import Content from './subComponents/Content/Content.vue'
 import Stats from './subComponents/Stats/Stats.vue'
-import { toRefs } from 'vue'
+import { toRefs, computed } from 'vue'
+import { getProfileUrl, getTweetUrl } from '~/modules/tweets/utils/navigation'
 
 const props = defineProps<{
     tweet: TweetType
@@ -41,14 +43,15 @@ const props = defineProps<{
 
 const { id, content, user, stats, type, createdAt, updatedAt } = toRefs(props.tweet)
 
-const navigateToTweet = () => {
-    navigateTo(`/tweet/${id.value}`)
-}
+// Use utility functions for URLs
+const profileUrl = computed(() => getProfileUrl(user.value))
+const tweetUrl = computed(() => getTweetUrl(props.tweet))
 
-const navigateToUser = (event: Event) => {
-    event.stopPropagation()
-    console.log('Navigating to user:', user.value)
-    navigateTo(`/${user.value.username}`)
+const navigateToTweet = () => {
+    if (tweetUrl.value !== '#') {
+        console.log('Navigating to tweet:', tweetUrl.value)
+        navigateTo(tweetUrl.value)
+    }
 }
 
 </script>
