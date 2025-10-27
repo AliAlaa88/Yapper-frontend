a1889c0a8235cc9c9c5d91594132c45f429e03e9
Amrhanysayed <amrhanyseed@gmail.com>
2025-10-27 21:53:20 +0300

    feat: add tabs component in common module

---
+++ app/modules/Common/components/Tabs/Tabs.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="w-full border-b border-primary">
+        <ul class="flex">
+            <li
+                v-for="tab in tabs"
+                :key="tab.value"
+                class="flex-1 text-center cursor-pointer transition-all duration-200 hover:bg-hover "
+                @click="handleChange(tab.value)"
+            >
+                <button
+                    class="relative w-full px-4 py-4 text-base font-medium transition-colors duration-200"
+                    :class="
+                        activeTab === tab.value ? 'text-primary' : 'text-muted hover:text-primary'
+                    "
+                >
+                    {{ tab.label }}
+                    <span
+                        v-if="activeTab === tab.value"
+                        class="absolute bottom-0 left-1/2 -translate-x-1/2 w-14 h-1 bg-blue rounded-full"
+                    />
+                </button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup lang="ts">
+interface Tab {
+    label: string
+    value: string
+}
+
+interface Props {
+    tabs: Tab[]
+    activeTab: string
+    onChange: (tab: string) => void
+}
+
+const { tabs, activeTab, onChange } = defineProps<Props>()
+
+const handleChange = (tab: string) => {
+    onChange(tab)
+}
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,12 +1,14 @@
 <template>
-    <div>
+    <div class="flex flex-col gap-4">
         <h1 class="text-primary">User</h1>
         <p class="text-primary">{{ data?.map((user: any) => user.name).join(', ') }}</p>
+        <Tabs :tabs="tabs" :activeTab="activeTab" @change="handleChange" />
     </div>
 </template>
 
 <script setup lang="ts">
 import { useQuery } from '@tanstack/vue-query'
+import Tabs from '../modules/Common/components/Tabs'
 
 definePageMeta({
     layout: 'main-layout',
@@ -16,6 +18,23 @@ async function getUser() {
     const response = await fetch('https://jsonplaceholder.typicode.com/users')
     const data = await response.json()
     return data
+}
+
+const tabs = [
+    {
+        label: 'User',
+        value: 'user',
+    },
+    {
+        label: 'Post',
+        value: 'post',
+    },
+]
+
+const activeTab = ref('user')
+
+function handleChange(tab: string) {
+    activeTab.value = tab
 }
 
 const { data, isLoading, error } = useQuery({
