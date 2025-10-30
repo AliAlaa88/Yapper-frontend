f3fe3a60d60a25886555156dbf3d09cc4e127144
hagar3bdelsalam <ha2ar3bdelsalam@gmail.com>
2025-10-30 16:19:39 +0300

    test(profile): add unit tests for ProfileMuteMessage component and fix related imports and paths

--- app/modules/profile/components/ProfileHeader/SubComponents/ProfileMuteMessage.vue
+++ app/modules/profile/components/ProfileHeader/SubComponents/ProfileMuteMessage.vue
@@ -16,10 +16,14 @@
 </template>
 
 <script setup lang="ts">
-import { useUserInfo } from '~/modules/profile/composables/useUserInfo'
-import { useUserInteractions } from '~/modules/profile/composables/useUserInteractions'
+import { useUserInfo } from '../../../composables/useUserInfo'
+import { useUserInteractions } from '../../../composables/useUserInteractions'
+import { inject, ref, watch } from 'vue'
 
 const userId = inject<Ref<string>>('user-id')!
+if (!userId) {
+    throw new Error('Missing required provide: user-id')
+}
 const { isMuted } = useUserInfo(userId)
 
 const showMuteMessage = ref(false)
