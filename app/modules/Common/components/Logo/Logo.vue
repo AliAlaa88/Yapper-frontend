a441a689f1b93d3085e0f4d7f697949af0e441b9
bedosaber77 <bedosaber77@gmail.com>
2025-10-31 23:52:27 +0200

    feat(logo): switch logo image based on dark/light mode

--- app/modules/Common/components/Logo/Logo.vue
+++ app/modules/Common/components/Logo/Logo.vue
@@ -1,12 +1,22 @@
 <template>
     <div :class="divClass">
-        <img src="~/assets/images/logo-white.png" alt="X Logo" :class="imgClass" />
+        <img v-if="themecomputed" src="~/assets/images/logo-white.png" alt="X Logo" :class="imgClass" />
+        <img v-else src="~/assets/images/logo.png" alt="X Logo" :class="imgClass" />
     </div>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue'
+
 defineProps<{
     imgClass?: string
     divClass?: string
 }>()
+
+const themecomputed = computed(() => {
+    if (typeof window !== 'undefined' && window.matchMedia) {
+        return window.matchMedia('(prefers-color-scheme: dark)').matches
+    }
+    return false
+})
 </script>
