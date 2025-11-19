a9b2280b82472265b7fe41e1eb08e8fc07e515fd
bedosaber77 <bedosaber77@gmail.com>
2025-11-19 09:50:15 +0200

    refactor(publisher): use CustomToolTip and simplify tooltip rendering

--- app/modules/tweets/components/Tweet/subComponents/Publisher/Publisher.vue
+++ app/modules/tweets/components/Tweet/subComponents/Publisher/Publisher.vue
@@ -1,72 +1,19 @@
 <template>
     <!-- Tweet Details View - Name above username -->
-    <TooltipProvider :delay-duration="400" :skip-delay-duration="0">
-        <div v-if="isDetail" class="mb-4">
-            <div class="flex items-center gap-3 mb-4">
-                <Tooltip>
-                    <TooltipTrigger as-child>
-                        <NuxtLink :id="`publisher-avatar-link-${id}`" :to="linkComputed">
-                            <img
-                                :id="`publisher-avatar-${id}`"
-                                :src="avatar_url"
-                                :alt="name"
-                                class="w-12 h-12 rounded-full hover:opacity-90 transition-opacity"
-                            />
-                        </NuxtLink>
-                    </TooltipTrigger>
-                    <TooltipContent class="p-0">
-                        <UserCard
-                            :id="id"
-                            :name="name"
-                            :username="username"
-                            :avatar="avatar_url"
-                            :bio="publisher.bio"
-                            :followers-count="publisher.followers"
-                            :following-count="publisher.following"
+    <div v-if="isDetail" class="mb-4">
+        <div class="flex items-center gap-3 mb-4">
+            <CustomToolTip :delay-duration="300" content-class="rounded-2xl shadow-xl border border-x-border">
+                <template #trigger>
+                    <NuxtLink :id="`publisher-avatar-link-${id}`" :to="linkComputed">
+                        <img
+                            :id="`publisher-avatar-${id}`"
+                            :src="avatar_url"
+                            :alt="name"
+                            class="w-12 h-12 rounded-full hover:opacity-90 transition-opacity"
                         />
-                    </TooltipContent>
-                </Tooltip>
-                <div class="flex flex-col">
-                    <Tooltip>
-                        <TooltipTrigger as-child>
-                            <NuxtLink
-                                :id="`publisher-name-link-${id}`"
-                                :to="linkComputed"
-                                class="font-bold text-[var(--color-x-black)] hover:underline text-[20px] leading-6"
-                            >
-                                {{ name }}
-                            </NuxtLink>
-                        </TooltipTrigger>
-                        <TooltipContent class="p-0">
-                            <UserCard
-                                :id="id"
-                                :name="name"
-                                :username="username"
-                                :avatar="avatar_url"
-                                :bio="publisher.bio"
-                                :followers-count="publisher.followers"
-                                :following-count="publisher.following"
-                            />
-                        </TooltipContent>
-                    </Tooltip>
-                    <span class="text-[var(--color-x-gray-dark)] text-[15px]">@{{ username }}</span>
-                </div>
-            </div>
-        </div>
-
-        <!-- Timeline View - Name and username inline -->
-        <div v-else class="flex items-center gap-1 mb-0.5">
-            <Tooltip>
-                <TooltipTrigger as-child>
-                    <NuxtLink
-                        :id="`publisher-name-link-timeline-${id}`"
-                        :to="linkComputed"
-                        class="font-bold text-[var(--color-x-black)] hover:underline text-[15px]"
-                    >
-                        {{ name }}
                     </NuxtLink>
-                </TooltipTrigger>
-                <TooltipContent class="p-0">
+                </template>
+                <template #content>
                     <UserCard
                         :id="id"
                         :name="name"
@@ -76,15 +23,66 @@
                         :followers-count="publisher.followers"
                         :following-count="publisher.following"
                     />
-                </TooltipContent>
-            </Tooltip>
-            <span class="text-x-gray-dark text-[15px]">@{{ username }}</span>
-            <span class="text-x-gray-dark text-[15px]">·</span>
-            <span class="text-x-gray-dark text-[15px] hover:underline cursor-pointer">
-                {{ formatDate(createdAt) }}
-            </span>
+                </template>
+            </CustomToolTip>
+            <div class="flex flex-col">
+                <CustomToolTip :delay-duration="300" content-class="rounded-2xl shadow-xl border border-x-border">
+                    <template #trigger>
+                        <NuxtLink
+                            :id="`publisher-name-link-${id}`"
+                            :to="linkComputed"
+                            class="font-bold text-[var(--color-x-black)] hover:underline text-[20px] leading-6"
+                        >
+                            {{ name }}
+                        </NuxtLink>
+                    </template>
+                    <template #content>
+                        <UserCard
+                            :id="id"
+                            :name="name"
+                            :username="username"
+                            :avatar="avatar_url"
+                            :bio="publisher.bio"
+                            :followers-count="publisher.followers"
+                            :following-count="publisher.following"
+                        />
+                    </template>
+                </CustomToolTip>
+                <span class="text-[var(--color-x-gray-dark)] text-[15px]">@{{ username }}</span>
+            </div>
         </div>
-    </TooltipProvider>
+    </div>
+
+    <!-- Timeline View - Name and username inline -->
+    <div v-else class="flex items-center gap-1 mb-0.5">
+        <CustomToolTip :delay-duration="300" content-class="rounded-2xl shadow-xl border border-x-border">
+            <template #trigger>
+                <NuxtLink
+                    :id="`publisher-name-link-timeline-${id}`"
+                    :to="linkComputed"
+                    class="font-bold text-[var(--color-x-black)] hover:underline text-[15px]"
+                >
+                    {{ name }}
+                </NuxtLink>
+            </template>
+            <template #content>
+                <UserCard
+                    :id="id"
+                    :name="name"
+                    :username="username"
+                    :avatar="avatar_url"
+                    :bio="publisher.bio"
+                    :followers-count="publisher.followers"
+                    :following-count="publisher.following"
+                />
+            </template>
+        </CustomToolTip>
+        <span class="text-x-gray-dark text-[15px]">@{{ username }}</span>
+        <span class="text-x-gray-dark text-[15px]">·</span>
+        <span class="text-x-gray-dark text-[15px] hover:underline cursor-pointer">
+            {{ formatDate(createdAt) }}
+        </span>
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -92,12 +90,7 @@ import { computed, toRefs } from 'vue'
 import type { User as UserType } from '../../../../../Common/types/user'
 import { formatDate } from '../../../../utils/lib'
 import { getProfileUrl } from '../../../../utils/navigation'
-import {
-    Tooltip,
-    TooltipContent,
-    TooltipProvider,
-    TooltipTrigger,
-} from '~/modules/Common/components/Tooltip'
+import { CustomToolTip } from '~/modules/Common/components/Tooltip'
 import UserCard from './UserCard.vue'
 
 const props = defineProps<{
