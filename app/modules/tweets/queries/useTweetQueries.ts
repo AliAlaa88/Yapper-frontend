215d4632bbfcdb53d4ce8427c133a9fe18d075df
bedosaber77 <bedosaber77@gmail.com>
2025-11-19 22:43:34 +0200

    feat(tweet-queries): add staleTime and gcTime for tweets cache

--- app/modules/tweets/queries/useTweetQueries.ts
+++ app/modules/tweets/queries/useTweetQueries.ts
@@ -25,6 +25,8 @@ export function useTweetsQuery(path: MaybeRef<string>) {
             return lastPage?.nextCursor ?? undefined
         },
         initialPageParam: '',
+        staleTime: 1000 * 60 * 5, // 5 minutes
+        gcTime: 1000 * 60 * 10, // 10 minutes
     })
 }
 
