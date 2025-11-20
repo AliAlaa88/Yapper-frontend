d7bcc857bb3dfa6b802ad7a673d1ba022f4c8d56
hagar3bdelsalam <ha2ar3bdelsalam@gmail.com>
2025-11-20 21:18:13 +0200

    feat(settings): add change age to account informations

---
+++ app/modules/settings/components/ChangeAge.vue
@@ -0,0 +1,34 @@
+<template>
+    <DetailedPanel title="Age">
+        <div class="w-full text-primary">
+            <div class="relative w-full pb-2 px-5 pt-2">
+                <p class="text-muted text-[13px] mt-0.5">
+                    If you haven’t provided a date of birth, we’ve provided an age range based on your Yapper profile and
+                    activity. Age information is used to personalize your experience.
+                </p>
+            </div>
+            <div class="border-t border-b border-primary px-5 py-3">
+                <p v-if="user.birth_date">{{ age }}</p>
+                <p v-else>Age not provided</p>
+            </div>
+            <div class="relative w-full pb-2 px-5 pt-2">
+                <p class="text-muted text-[14px] mt-0.5">
+                    Not right? You can add your date of birth to your
+                    <NuxtLink to="/settings/profile" class="text-accent hover:underline">
+                        profile
+                    </NuxtLink>
+                    without sharing it publicly.
+                </p>
+            </div>
+        </div>
+    </DetailedPanel>
+</template>
+
+<script setup lang="ts">
+import DetailedPanel from './DetailedPanel.vue'
+import type { User } from '~/modules/Common/types/user'
+import { getUser } from '~/utils/helpers'
+import { calculateAge } from '../utils/calculations'
+const user = getUser() as User
+const age = computed(() => calculateAge(user.birth_date ?? ''))
+</script>
