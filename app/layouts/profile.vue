ec6d36781e985d218a85bc79f46d52a2a4c46493
AliElDin Alaa <147806061+AliAlaa88@users.noreply.github.com>
2025-11-23 16:43:10 +0200

    fix: profile not found

--- app/layouts/profile.vue
+++ app/layouts/profile.vue
@@ -1,8 +1,10 @@
 <template>
     <MainLayout>
-        <ProfileHeader />
-
-        <NuxtPage :key="username" />
+        <ProfileNotFound v-if="showNotFound" />
+        <template v-else>
+            <ProfileHeader />
+            <NuxtPage :key="username" />
+        </template>
 
         <SnackBar />
         <ConfirmtionModal />
@@ -11,18 +13,20 @@
 
 <script setup lang="ts">
 import ProfileHeader from '../modules/profile/components/ProfileHeader/ProfileHeader.vue'
+import ProfileNotFound from '../modules/profile/components/ProfileNotFound.vue'
 import SnackBar from '../modules/profile/components/ProfileContent/SubComponents/SnackBar.vue'
 import ConfirmtionModal from '~/modules/profile/components/ProfileHeader/SubComponents/ConfirmtionModal.vue'
 import { useProfile } from '~/modules/profile/composables/useProfile'
 import { useProfileStore } from '~/modules/profile/stores/profileStore'
 import { useProfileProviders } from '~/modules/profile/composables/useProfileProviders'
-import { watch } from 'vue'
+import { watch, ref } from 'vue'
 import MainLayout from './main-layout.vue'
 
 useProfileProviders()
 
 const route = useRoute()
 const username = computed(() => route.params.username as string)
+const showNotFound = ref(false)
 
 const profileStore = useProfileStore()
 
@@ -31,7 +35,14 @@ watch(
     (newUsername) => {
         if (newUsername) {
             profileStore.clearProfile()
-            useProfile(newUsername)
+            showNotFound.value = false
+            const { error } = useProfile(newUsername)
+
+            watch(error, (newError) => {
+                if (newError && newError.message === 'User not found') {
+                    showNotFound.value = true
+                }
+            }, { immediate: true })
         }
     },
     { immediate: true },
