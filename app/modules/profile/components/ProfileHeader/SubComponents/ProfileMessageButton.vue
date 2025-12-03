971436a52513661f4c85f257af9d8b92a4d79e39
Amrhanysayed <amrhanyseed@gmail.com>
2025-12-03 10:06:21 +0200

    feat(ProfileMessageButton): update message button logic to navigate to user-specific messages

--- app/modules/profile/components/ProfileHeader/SubComponents/ProfileMessageButton.vue
+++ app/modules/profile/components/ProfileHeader/SubComponents/ProfileMessageButton.vue
@@ -1,6 +1,6 @@
 <template>
     <button
-        class="flex h-9 w-9 items-center justify-center rounded-full border border-[#536471] transition hover:bg-[#181818]"
+        class="flex h-9 w-9 items-center cursor-pointer justify-center rounded-full border border-[#536471] transition hover:bg-[#181818]"
         @click="goToMessages"
     >
         <Mail :size="18" class="text-primary" />
@@ -9,10 +9,17 @@
 
 <script setup lang="ts">
 import { Mail } from 'lucide-vue-next'
+import { useProfileStore } from '~/modules/profile/stores/profileStore'
 
 const router = useRouter()
+const profileStore = useProfileStore()
 
 function goToMessages() {
-    router.push('/messages')
+    const userId = profileStore.getProfileId()
+    if (userId) {
+        router.push(`/messages/${userId}`)
+    } else {
+        router.push('/messages')
+    }
 }
 </script>
