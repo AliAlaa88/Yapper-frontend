22581e610dd343fabc245969f9c515012db90ce1
hagar3bdelsalam <ha2ar3bdelsalam@gmail.com>
2025-12-03 20:25:44 +0200

    feat: navigate to user profile from muted and blocked lists

--- app/modules/settings/components/MuteAndBlock/SubComponents/UserAccountItem.vue
+++ app/modules/settings/components/MuteAndBlock/SubComponents/UserAccountItem.vue
@@ -1,17 +1,19 @@
 <template>
-    <div class="flex items-start px-4 py-3 transition">
-        <img :src="account.avatar_url ?? ''" class="w-12 h-12 rounded-full object-cover" />
+    <div class="flex items-start px-4 py-3 transition hover:bg-hover">
+        <NuxtLink :to="`/${account.username}`" class="flex items-start w-full">
+            <img :src="account.avatar_url ?? ''" class="w-12 h-12 rounded-full object-cover" >
 
-        <div class="flex-1 ml-3 space-y-1">
-            <div class="flex items-left flex-col justify-between">
-                <span class="font-semibold">{{ account.name }}</span>
-                <p class="text-muted text-[14px]">@{{ account.username }}</p>
+            <div class="flex-1 ml-3 space-y-1">
+                <div class="flex items-left flex-col justify-between">
+                    <span class="font-semibold hover:underline">{{ account.name }}</span>
+                    <p class="text-muted text-[14px]">@{{ account.username }}</p>
+                </div>
+                <p class="text-sm leading-tight text-primary whitespace-pre-line">
+                    {{ account.bio }}
+                </p>
             </div>
-            <p class="text-sm leading-tight text-primary whitespace-pre-line">
-                {{ account.bio }}
-            </p>
-        </div>
 
+        </NuxtLink>
         <slot />
     </div>
 </template>
