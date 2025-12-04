8922438f7d83d801fc98febd030e17a0cc46c92e
bedosaber77 <bedosaber77@gmail.com>
2025-12-04 16:29:08 +0200

    feat(Publisher): pass isOpen to UserCard in all tooltip instances

--- app/modules/tweets/components/Tweet/subComponents/Publisher/Publisher.vue
+++ app/modules/tweets/components/Tweet/subComponents/Publisher/Publisher.vue
@@ -17,7 +17,7 @@
                         />
                     </NuxtLink>
                 </template>
-                <template #content>
+                <template #content="{ isOpen }">
                     <UserCard
                         :id="id"
                         :name="name"
@@ -27,6 +27,7 @@
                         :followers-count="publisher.followers"
                         :following-count="publisher.following"
                         :is_following="publisher.is_following"
+                        :is-open="isOpen"
                     />
                 </template>
             </CustomToolTip>
@@ -44,7 +45,7 @@
                             {{ name }}
                         </NuxtLink>
                     </template>
-                    <template #content>
+                    <template #content="{ isOpen }">
                         <UserCard
                             :id="id"
                             :name="name"
@@ -54,6 +55,7 @@
                             :followers-count="publisher.followers"
                             :following-count="publisher.following"
                             :is_following="publisher.is_following"
+                            :is-open="isOpen"
                         />
                     </template>
                 </CustomToolTip>
@@ -77,7 +79,7 @@
                     {{ name }}
                 </NuxtLink>
             </template>
-            <template #content>
+            <template #content="{ isOpen }">
                 <UserCard
                     :id="id"
                     :name="name"
@@ -87,6 +89,7 @@
                     :followers-count="publisher.followers"
                     :following-count="publisher.following"
                     :is_following="publisher.is_following"
+                    :is-open="isOpen"
                 />
             </template>
         </CustomToolTip>
