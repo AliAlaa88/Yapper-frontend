4bbccbdbf8528c8917ab6b157328af7b52578a7c
AliElDin Alaa <147806061+AliAlaa88@users.noreply.github.com>
2025-12-07 20:03:09 +0200

    feat: seo + issues

--- app/pages/[username]/followers.vue
+++ app/pages/[username]/followers.vue
@@ -5,36 +5,8 @@
 
 <script setup lang="ts">
 import FollowLists from '../../modules/profile/components/FollowLists/FollowLists.vue'
-import { useProfileStore } from '~/modules/profile/stores/profileStore'
-import { useSeo } from '~/modules/Common/composables/useSeo'
-
-const { t } = useI18n()
-const route = useRoute()
-const profileStore = useProfileStore()
-const { profile } = storeToRefs(profileStore)
-
-// Set initial SEO with username from route
-const username = route.params.username as string
-useSeo({
-    title: `People following @${username} / Yapper`,
-    description: t('seo.default.description'),
-    keywords: t('seo.default.keywords'),
-})
-
-// Update SEO when profile data loads
-watch(
-    profile,
-    (newProfile) => {
-        if (newProfile) {
-            useSeo({
-                title: t('seo.followers.title', { name: newProfile.name, username: newProfile.username }),
-                description: t('seo.followers.description', { name: newProfile.name }),
-                keywords: t('seo.followers.keywords', { username: newProfile.username }),
-            })
-        }
-    },
-    { immediate: true }
-)
+import { useProfilePageSeo } from '~/modules/profile/composables/useProfilePageSeo'
+useProfilePageSeo('followers')
 
 definePageMeta({
     layout: 'main-layout',
--- app/pages/[username]/index.vue
+++ app/pages/[username]/index.vue
@@ -5,37 +5,8 @@
 
 <script setup lang="ts">
 import ProfileContent from '../../modules/profile/components/ProfileContent/ProfileContent.vue'
-import { useProfileStore } from '~/modules/profile/stores/profileStore'
-import { useSeo, useProfileSeo } from '~/modules/Common/composables/useSeo'
-
-const { t } = useI18n()
-const route = useRoute()
-const profileStore = useProfileStore()
-const { profile } = storeToRefs(profileStore)
-
-// Set initial SEO with username from route
-const username = route.params.username as string
-useSeo({
-    title: `@${username} / Yapper`,
-    description: t('seo.default.description'),
-    keywords: t('seo.default.keywords'),
-})
-
-// Update SEO when profile data loads
-watch(
-    profile,
-    (newProfile) => {
-        if (newProfile) {
-            useProfileSeo({
-                name: newProfile.name,
-                username: newProfile.username,
-                bio: newProfile.bio,
-                profile_image: newProfile.profile_image,
-            })
-        }
-    },
-    { immediate: true }
-)
+import { useProfilePageSeo } from '~/modules/profile/composables/useProfilePageSeo'
+useProfilePageSeo('profile')
 
 definePageMeta({
     layout: 'profile',
