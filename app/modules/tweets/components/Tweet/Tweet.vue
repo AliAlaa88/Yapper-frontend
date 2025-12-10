1320d0f18c3f8bd6cce3ddd9bb7ed8adf1ba7840
Abdelrahman Medhat Saber <136710727+bedosaber77@users.noreply.github.com>
2025-12-10 20:11:25 +0200

    Refactor(tweet): Adjust `content` computed property for `parentTweet` logic

--- app/modules/tweets/components/Tweet/Tweet.vue
+++ app/modules/tweets/components/Tweet/Tweet.vue
@@ -235,10 +235,7 @@ const content = computed(() => ({
     text: props.tweet.content,
     images: props.tweet.images || [],
     videos: props.tweet.videos || [],
-    parentTweet:
-        props.tweet.type !== 'reply'
-            ? (props.tweet.parent_tweet ?? props.tweet.quoted_tweet)
-            : undefined,
+    parentTweet: props.tweet.type === 'quote' ? props.tweet.parent_tweet : undefined,
 }))
 
 // Transform user to include avatar property
