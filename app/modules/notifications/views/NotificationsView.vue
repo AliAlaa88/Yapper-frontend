68c0a89e4148f5b661b4d31059ad14edda8fe257
AliElDin Alaa <147806061+AliAlaa88@users.noreply.github.com>
2025-12-12 11:58:06 +0200

    fix: fixed tabs

--- app/modules/notifications/views/NotificationsView.vue
+++ app/modules/notifications/views/NotificationsView.vue
@@ -1,30 +1,45 @@
 <template>
     <div class="flex flex-col w-full min-h-screen">
-        <h2 class="text-lg font-bold text-primary px-4 py-3">{{ $t('notifications.title') }}</h2>
-        <Tabs :tabs="tabs" :active-tab="activeTab" @change="handleChange" />
+        <div class="sticky top-0 z-10 bg-primary/80 backdrop-blur-md">
+            <h2 class="text-lg font-bold text-primary px-4 py-3">
+                {{ $t('notifications.title') }}
+            </h2>
+            <Tabs :tabs="tabs" :active-tab="activeTab" @change="handleChange" />
+        </div>
         <div
             v-if="activeTab === 'mentions' ? isLoadingMentions : isLoadingNotifications"
-            class="flex justify-center mt-9">
+            class="flex justify-center mt-9"
+        >
             <LoadingSpinner />
         </div>
         <div
-            v-if="activeTab === 'mentions'
-                ? isSuccessfullyMentions && mentions.length > 0
-                : isSuccessfullyNotifications && notifications.length > 0">
+            v-if="
+                activeTab === 'mentions'
+                    ? isSuccessfullyMentions && mentions.length > 0
+                    : isSuccessfullyNotifications && notifications.length > 0
+            "
+        >
             <NotificationsList
                 :notifications="listData.value"
-                :is-fetching-next-page="activeTab === 'all' ? isFetchingNextNotifications : isFetchingNextMentions"
+                :is-fetching-next-page="
+                    activeTab === 'all' ? isFetchingNextNotifications : isFetchingNextMentions
+                "
                 :has-next-page="activeTab === 'all' ? hasNextNotifications : hasNextMentions"
                 :fetch-next-page="activeTab === 'all' ? fetchNextNotifications : fetchNextMentions"
             />
         </div>
         <div
-            v-else-if="activeTab === 'mentions'
-                ? isSuccessfullyMentions && mentions.length === 0
-                : isSuccessfullyNotifications && notifications.length === 0"
-            class="flex justify-center items-center flex-col space-y-2 mt-9">
+            v-else-if="
+                activeTab === 'mentions'
+                    ? isSuccessfullyMentions && mentions.length === 0
+                    : isSuccessfullyNotifications && notifications.length === 0
+            "
+            class="flex justify-center items-center flex-col space-y-2 mt-9"
+        >
             <h1 class="font-bold text-3xl text-primary">{{ $t('notifications.empty.title') }}</h1>
-            <p v-if="activeTab === 'mentions'" class="text-muted text-sm">{{ $t('notifications.empty.mentions_description') }}</p>
+            <p v-if="activeTab === 'mentions'" class="text-muted text-sm">
+                {{ $t('notifications.empty.mentions_description') }}
+            </p>
             <p v-else class="text-muted text-sm">{{ $t('notifications.empty.all_description') }}</p>
         </div>
     </div>
@@ -42,7 +57,7 @@ import { useGetMentionsQuery } from '../queries/useGetMentionsQuery'
 const { $notificationsSocketService } = useNuxtApp()
 const activeTab = ref('all')
 
-const listData = computed(() => activeTab.value === 'all' ? notifications : mentions)
+const listData = computed(() => (activeTab.value === 'all' ? notifications : mentions))
 
 const {
     notifications,
@@ -67,7 +82,6 @@ console.log('list data', listData)
 const router = useRouter()
 const route = useRoute()
 const userStore = useUserStore()
-
 
 // const hasScrolled = ref(false)
 
@@ -106,12 +120,15 @@ onBeforeRouteLeave((to, from, next) => {
 })
 
 // browser back/forward
-watch(() => route.path, (newPath, oldPath) => {
-    if (oldPath === '/notifications' && newPath !== '/notifications') {
-        console.log('[Notifications] Route changed → marking as seen')
-        $notificationsSocketService.markNotificationsAsSeen()
-    }
-})
+watch(
+    () => route.path,
+    (newPath, oldPath) => {
+        if (oldPath === '/notifications' && newPath !== '/notifications') {
+            console.log('[Notifications] Route changed → marking as seen')
+            $notificationsSocketService.markNotificationsAsSeen()
+        }
+    },
+)
 
 watch(activeTab, () => {
     window.scrollTo({ top: 0, behavior: 'smooth' })
@@ -130,9 +147,7 @@ const tabs = computed(() => [
     },
 ])
 
-
 function handleChange(tab: string) {
     activeTab.value = tab
 }
 </script>
-
