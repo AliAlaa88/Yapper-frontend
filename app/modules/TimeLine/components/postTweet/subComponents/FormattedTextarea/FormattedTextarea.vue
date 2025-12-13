99165a7f121ff82ed6fa16115c5e619f667da5e9
Abdelrahman Medhat Saber <136710727+bedosaber77@users.noreply.github.com>
2025-12-13 17:55:55 +0200

    chore: minor code and type consistency improvements

--- app/modules/TimeLine/components/postTweet/subComponents/FormattedTextarea/FormattedTextarea.vue
+++ app/modules/TimeLine/components/postTweet/subComponents/FormattedTextarea/FormattedTextarea.vue
@@ -9,23 +9,23 @@
         >
             <span v-if="!modelValue" class="text-muted">{{ placeholder }}</span>
 
-            <span v-else v-html="formattedContent"></span>
+            <span v-else v-html="formattedContent" />
             <br v-if="modelValue.endsWith('\n')" />
         </div>
 
         <textarea
+            :id="id"
             ref="textareaRef"
             :value="modelValue"
-            @input="handleInput"
-            @scroll="handleScroll"
-            :id="id"
             :placeholder="!modelValue ? placeholder : ''"
             :dir="textDirection"
             class="relative w-full min-h-24 p-4 bg-transparent text-transparent caret-black dark:caret-white resize-none focus:outline-none whitespace-pre-wrap break-words overflow-y-hidden text-base leading-normal font-sans placeholder:text-transparent z-20 selection:bg-blue-200/30"
             :class="props.inlineborder ? 'border-b border-primary focus:border-accent' : ''"
             style="unicode-bidi: plaintext"
             spellcheck="false"
-        ></textarea>
+            @input="handleInput"
+            @scroll="handleScroll"
+        />
 
         <MentionSuggestion
             :visible="showMentionPopup"
