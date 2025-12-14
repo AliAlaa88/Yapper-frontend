0867a4c2fb4cd3a5a7e73a09e2a292c5a002fe2b
AliElDin Alaa <147806061+AliAlaa88@users.noreply.github.com>
2025-12-14 15:44:09 +0200

    fix: responsive issues

--- app/modules/profile/components/ProfileHeader/SubComponents/ProfileEditButton.vue
+++ app/modules/profile/components/ProfileHeader/SubComponents/ProfileEditButton.vue
@@ -1,7 +1,7 @@
 <template>
     <button
         id="btn-edit-profile"
-        class="cursor-pointer font-bold text-[15px] leading-5 flex items-center justify-center whitespace-nowrap rounded-full transition-colors duration-200 bg-transparent border border-[#536471] text-primary px-4 py-1.5 h-9 hover:bg-[#181818] mb-3"
+        class="cursor-pointer font-bold text-[15px] leading-5 flex items-center justify-center whitespace-nowrap rounded-full transition-colors duration-200 bg-transparent border border-[#536471] text-primary px-4 py-1.5 h-9 hover:bg-[#181818] md:mb-3"
         @click="handleEditProfile"
     >
         {{ $t('profile.editProfile') }}
